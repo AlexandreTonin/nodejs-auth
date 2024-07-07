126af92caea75990150d869220a4a4e8ab7e82d2
AlexandreTonin <xandetonin@gmail.com>
2024-07-07 18:50:25 -0300

    Registration logic created in AuthController

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,5 +1,48 @@
+import bcrypt from 'bcrypt'
+import { UserModel } from '../models/User.js'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export class AuthController {
-    static async register(req, res) { }
+    static async register(req, res) {
+        const { firstName, lastName, email, password } = req.body
+
+        if (!firstName) {
+            return res.status(422).json({ message: "First name cannot be empty" })
+        }
+
+        if (!lastName) {
+            return res.status(422).json({ message: "Last name cannot be empty" })
+        }
+
+        if (!email) {
+            return res.status(422).json({ message: "Email cannot be empty" })
+        }
+
+        if (!password) {
+            return res.status(422).json({ message: "Password cannot be empty" })
+        }
+
+        try {
+            const emailExists = await UserModel.findByEmail(email)
+
+            if (emailExists.length > 0) {
+                return res.status(422).json({ message: "Email already registered" })
+            }
+
+            const hashedPassword = await bcrypt.hashSync(password, 10)
+
+            const newUserId = await UserModel.create(firstName, lastName, email, hashedPassword)
+
+            const token = jwt.sign({ userId: newUserId }, process.env.JWT_SECRET, { expiresIn: '1h' })
+
+            return res.status(201).json({ message: "Registered sucessfully", id: newUserId, token })
+        } catch (error) {
+            return res.status(500).json({ message: "Register failed" })
+        }
+    }
     static async login(req, res) { }
     static async logout(req, res) { }
 }
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,32 @@
+import { connection } from "../db/db.js";
+
 export class UserModel {
     static async findAll() { }
     static async findById(userId) { }
-    static async findByEmail(userEmail) { }
+    static async findByEmail(userEmail) {
+        const sql = 'SELECT id, firstName, lastName, email, isDeleted, createdAt, modifiedAt, createdBy, modifiedBy FROM `user` WHERE `email` = ? AND isDeleted = ?';
+        const values = [userEmail, 0]
+
+        try {
+            const [rows, fields] = await connection.query(sql, values)
+            return rows
+        } catch (error) {
+            console.log(error)
+            return error
+        }
+    }
     static async findByName(userFirstName, userLastName) { }
-    static async create(userFirstName, userLastName, userEmail, userPassword) { }
+    static async create(userFirstName, userLastName, userEmail, userPassword) {
+        const sql = 'INSERT INTO `user` (firstName, lastName, email, passwordHash) VALUES (?, ?, ?, ?)';
+        const values = [userFirstName, userLastName, userEmail, userPassword]
+
+        try {
+            const [rows, fields] = await connection.execute(sql, values)
+            return rows.insertId
+        } catch (error) {
+            return error
+        }
+    }
     static async update(userId, data) { }
     static async delete(userId) { }
 }
